eaafbb28c56d0c84fa622def3c14afce4387bf2b
JoeGruff <joegruffins@gmail.com>
2019-04-04 12:37:43 +0900

    better errors

--- loopsandfunctions/loopsandfunctions.go
+++ loopsandfunctions/loopsandfunctions.go
@@ -7,48 +7,65 @@ import (
 
 const DELTA = 0.00000001
 
-func nextZandDiff(x,z float64) (float64, float64) {
-	newZ := z - (z*z - x) / (2*z)
+var (
+	enEnterString  string = `Enter number to square. "exit" to quit...`
+	enNegNumString string = `Can't square negative number.`
+	enAnswerString string = "Sqrt of %v is %v"
+	enNumberPlz    string = `Please enter a number.`
+)
+
+type SqrtNegativeError float64
+
+func (f SqrtNegativeError) Error() string {
+	return fmt.Sprintf(enNegNumString+" %v", float64(f))
+}
+
+func nextZandDiff(x, z float64) (float64, float64) {
+	newZ := z - (z*z-x)/(2*z)
 	diff := newZ - z
-	if diff < 0 { diff *= -1 }
+	if diff < 0 {
+		diff *= -1
+	}
 	return newZ, diff
 }
 
-func Sqrt(x float64) float64 {
-	z := 1.0
-	newZ, diff := nextZandDiff(x,z)
-	for ;diff > DELTA; z = newZ{
-		newZ, diff = nextZandDiff(x,z)
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, SqrtNegativeError(x)
 	}
-	return z
-}
-
-func printEnter(){
-	fmt.Println("Enter number to square. \"exit\" to quit...")
+	z := 1.0
+	newZ, diff := nextZandDiff(x, z)
+	for ; diff > DELTA; z = newZ {
+		newZ, diff = nextZandDiff(x, z)
+	}
+	return z, nil
 }
 
 func processInput(s string) (stop bool) {
 	stop = false
-	if num, err := strconv.ParseFloat(s,64); s == "exit" {
+	if num, err := strconv.ParseFloat(s, 64); s == "exit" {
 		stop = true
 	} else if err != nil {
-		fmt.Println("Please enter a number.")
-	} else if num < 0.0 {
-		fmt.Println("Only positive Numbers")
+		fmt.Println(enNumberPlz)
 	} else {
-		printEnter()
-		fmt.Println(enterString)
+		sqrt, err := Sqrt(num)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf(enAnswerString+"\n\n", num, sqrt)
+		fmt.Println(enEnterString)
 	}
 	return
 }
 
-
 func main() {
-	printEnter()
-	for{
+	fmt.Println(enEnterString)
+	for {
 		var input string
 		fmt.Scan(&input)
-		if processInput(input) { return }
+		if processInput(input) {
+			return
+		}
 	}
 }
-
