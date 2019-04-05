54eeb31360c2fa92d32c36e93056653ae5649f6d
JoeGruff <joegruffins@gmail.com>
2019-04-05 12:10:45 +0900

    back to one file, multiple difficult to use in online tour

--- loopsandfunctions/loopsandfunctions.go
+++ loopsandfunctions/loopsandfunctions.go
@@ -5,11 +5,40 @@ import (
 	"strconv"
 )
 
+const DELTA = 1e-10
+
 var (
 	enEnterString  string = `Enter number to square. "exit" to quit...`
 	enAnswerString string = "Sqrt of %v is %v"
 	enNumberPlz    string = `Please enter a number.`
+	enNegNumString string = `Can't square negative number.`
 )
+
+type SqrtNegativeError float64
+
+func (f SqrtNegativeError) Error() string {
+	return fmt.Sprintf(enNegNumString+" %v", float64(f))
+}
+
+func nextZandDiff(x, z float64) (float64, float64) {
+	newZ := z - (z*z-x)/(2*z)
+	diff := newZ - z
+	if diff < 0 {
+		diff = -diff
+	}
+	return newZ, diff
+}
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, SqrtNegativeError(x)
+	}
+	z := 1.0
+	for newZ, diff := nextZandDiff(x, z); diff > DELTA; z = newZ {
+		newZ, diff = nextZandDiff(x, z)
+	}
+	return z, nil
+}
 
 func processInput(s string) (stop bool) {
 	stop = false
