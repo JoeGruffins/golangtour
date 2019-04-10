2a3ec85f728c79a3589512f7a2205463dfc33345
JoeGruff <joegruffins@gmail.com>
2019-04-11 08:47:00 +0900

    refractoring

--- slices/slices.go
+++ slices/slices.go
@@ -2,23 +2,20 @@ package main
 
 import "golang.org/x/tour/pic"
 
-type myFunc struct {
-	F func(x, y int) int
-}
+type myFunc func(x, y int) int
 
-func (g myFunc) Pic(dx, dy int) [][]uint8 {
+func (f myFunc) Pic(dx, dy int) [][]uint8 {
 	pic := make([][]uint8, dy)
 	for y := range pic {
 		pic[y] = make([]uint8, dx)
 		for x := range pic[y] {
-			pic[y][x] = uint8(g.F(x, y))
+			pic[y][x] = uint8(f(x, y))
 		}
 	}
 	return pic
 }
 
 func main() {
-	p := new(myFunc)
-	p.F = func(x, y int) int { return x + y }
-	pic.Show(p.Pic)
+	f := myFunc(func(x, y int) int { return x + y })
+	pic.Show(f.Pic)
 }
