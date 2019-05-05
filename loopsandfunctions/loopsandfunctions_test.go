02bea33c56d433ab231c58c70c0ebfd7fec268a6
JoeGruff <joegruffins@gmail.com>
2019-05-05 21:00:17 +0900

    add stdout to input test

--- loopsandfunctions/loopsandfunctions_test.go
+++ loopsandfunctions/loopsandfunctions_test.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"math"
+	"os"
 	"testing"
 )
 
@@ -11,9 +15,10 @@ type sqrtTestTrio struct {
 	outputErr error
 }
 
-type inputTestPair struct {
-	input  string
-	output bool
+type inputTestTrio struct {
+	input        string
+	outputBool   bool
+	outputStdout string
 }
 
 var sqrtTests = []sqrtTestTrio{
@@ -22,14 +27,45 @@ var sqrtTests = []sqrtTestTrio{
 	{0.1, math.Sqrt(0.1), nil},
 	{0, 0, nil},
 	{-1, 0, SqrtNegativeError(-1)},
+	{outputNum: 0, outputErr: nil},
 }
 
-var inputTests = []inputTestPair{
-	{"exit", true},
-	{"eight", false},
-	{"7", false},
-	{"0", false},
-	{"-3", false},
+var inputTests = []inputTestTrio{
+	{"exit", true, ""},
+	{"eight", false, fmt.Sprintf(enNumberPlz + "\n")},
+	{"", false, fmt.Sprintf(enNumberPlz + "\n")},
+	{"quit", false, fmt.Sprintf(enNumberPlz + "\n")},
+	{`!"#$%&'()@<>`, false, fmt.Sprintf(enNumberPlz + "\n")},
+	{"54ee12", false, fmt.Sprintf(enNumberPlz + "\n")},
+	{"5e12", false, fmt.Sprintf(enAnswerString+"\n\n"+enEnterString+"\n", 5e12, sqrtNoError(5e12))},
+	//scientific notation with two digit significand causes hang...
+	//{"54e12", false, fmt.Sprintf(enAnswerString+"\n\n"+enEnterString+"\n", 54e12, sqrtNoError(54e12))},
+	{"7", false, fmt.Sprintf(enAnswerString+"\n\n"+enEnterString+"\n", 7, sqrtNoError(7))},
+	{"0", false, fmt.Sprintf(enAnswerString+"\n\n"+enEnterString+"\n", 0, 0)},
+	{"-3", false, fmt.Sprintf(SqrtNegativeError(-3).Error() + "\n")},
+}
+
+//helper func to capture stdout
+func captureOutput(f func()) string {
+	old := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+	f()
+	outC := make(chan string)
+	go func() {
+		var buf bytes.Buffer
+		io.Copy(&buf, r)
+		outC <- buf.String()
+	}()
+	w.Close()
+	os.Stdout = old
+	return <-outC
+}
+
+//helper function to ignore error
+func sqrtNoError(x float64) float64 {
+	v, _ := Sqrt(x)
+	return v
 }
 
 func TestSqrt(t *testing.T) {
@@ -42,10 +78,13 @@ func TestSqrt(t *testing.T) {
 }
 
 func TestInput(t *testing.T) {
-	for _, pair := range inputTests {
-		v := processInput(pair.input)
-		if v != pair.output {
-			t.Error("for", pair.input, "expected", pair.output, "got", v)
+	for _, trio := range inputTests {
+		var v bool
+		stdout := captureOutput(func() {
+			v = processInput(trio.input)
+		})
+		if v != trio.outputBool || stdout != trio.outputStdout {
+			t.Error("for", trio.input, "expected", trio.outputBool, trio.outputStdout, "got", v, stdout)
 		}
 	}
 }
